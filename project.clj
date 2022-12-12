8208c23b7423a06769fc3a905c93daba925007d3
Lauri Oherd <lauri.oherd@gmail.com>
2022-12-12 22:30:25 +0200

    update dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject banks2ledger "1.3.0"
   :description "Banks' CSV to ledger converter with probabilistic payment matching"
-  :url "https://tomszilagyi.github.io/payment-matching"
+  :url "https://github.com/laurio/banks2ledger"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [clojure-csv/clojure-csv "2.0.1"]]
+                 [clojure-csv/clojure-csv "2.0.2"]]
   :main ^:skip-aot banks2ledger.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
